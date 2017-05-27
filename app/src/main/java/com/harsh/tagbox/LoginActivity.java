4b6108d3a181ca6000c05a21b5d69f39f07aac54
Harsh <harsh@droidharsh.com>
2017-05-27 16:42:31 +0530

    Set toolbar.

--- app/src/main/java/com/harsh/tagbox/LoginActivity.java
+++ app/src/main/java/com/harsh/tagbox/LoginActivity.java
@@ -3,6 +3,7 @@ package com.harsh.tagbox;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 /**
  * A login screen that offers login via email/password.
@@ -12,6 +13,10 @@ public class LoginActivity extends AppCompatActivity  {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle("Login");
     }
 }
 
