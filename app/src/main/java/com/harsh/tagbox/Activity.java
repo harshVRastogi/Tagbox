b92eafa1bfb3004d7984c76d74fac8ec948eae6c
Harsh <harsh@droidharsh.com>
2017-05-28 17:17:27 +0530

    Sending notification on login success.

--- app/src/main/java/com/harsh/tagbox/Activity.java
+++ app/src/main/java/com/harsh/tagbox/Activity.java
@@ -1,7 +1,11 @@
 package com.harsh.tagbox;
 
+import android.app.Notification;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -13,5 +17,23 @@ public class Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity);
         setResult(Activity.RESULT_OK);
+        notifyLogin();
+    }
+
+    private void notifyLogin(){
+        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(this);
+        Uri soundUri = getRingtoneUri();
+        Notification.Builder notificationCompat = new Notification.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("Login")
+                .setContentText("Successful")
+                .setSound(soundUri)
+                .setVibrate(new long[]{0, 500});
+
+        managerCompat.notify(1, notificationCompat.build());
+    }
+
+    private Uri getRingtoneUri() {
+        return RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
     }
 }
